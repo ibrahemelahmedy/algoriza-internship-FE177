d75228770064c3af160849dcdba068c1386c0431
ibrahem Kewan <ibrahem.elahmedy@gmail.com>
2023-12-03 21:59:20 +0200

    sadasdas

--- src/views/notFound.vue
+++ src/views/notFound.vue
@@ -2,7 +2,10 @@
 
 <template>
 	<h1>404</h1>
-	<h2>Page Not Found</h2>
+	<h2
+		>Page Not Found get back to
+		<router-link :to="{ name: 'home' }">homePage</router-link></h2
+	>
 </template>
 
 <script>
